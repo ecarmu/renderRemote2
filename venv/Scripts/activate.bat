90d526565560129f1aba552c75bed2770562f71d
ecarmu <ardaharman2003@gmail.com>
2024-01-14 14:43:05 +0300

    Add Dockerfile and script

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ardah\OneDrive\Masaüstü\SE 3355\finalProjeBB\cloneBack\venv
+set VIRTUAL_ENV=C:\Users\ardah\OneDrive\Masaüstü\SE 3355\bitArtikkk\cloneBack\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
